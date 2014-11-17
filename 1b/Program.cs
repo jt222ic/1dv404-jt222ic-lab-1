b1e96bce45f9b1bcd8fbed96ef53f9e16e923ef2
jantran <jt222ic@lnu.student.se>
2014-11-17 20:14:04 +0100

    gegeeg

---
+++ 1b/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int odd= 0;            // variablar 
+            int even = 0;
+            int zero = 0;
+               
+
+            while (true )  // skapar en while sats för att det ska loopar för ConsoleKey " uträkning sker under knapptryck.
+            {
+
+                ConsoleKeyInfo input = Console.ReadKey();         // input med tilläg av ConsoleKeyinput -läsa in inmatningen
+                
+                if (input.Key== ConsoleKey.Enter)    // om inmatningen är lika med Enter så stängs konsolen av
+                { break; }
+
+                int number = int.Parse(input.KeyChar.ToString());  // värden som skrivs in som jag har omvandlat från keychar till string sedan int Parse.
+
+                if ( number == 0)    // om värden är lika med noll sker uträkning för 0
+                {
+                    zero+=1;
+
+                }
+                else
+                switch (number % 2)     /// uträkning nför jämna och udda
+
+                {
+
+                    case 0: 
+                        even +=1;
+                        break;
+                    case 1:
+
+                        odd +=1;
+                        break;
+                    }
+                    
+                Console.WriteLine("zero : {0} even : {1} odd : {2}", zero, even, odd);   // resultaten av uträkningen
+                
+
+            }
+
+        }
+    }
+}
---
+++ 1c/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max;                  // variablar för uträkning
+            int nextmax;
+            int input;
+            
+            Console.WriteLine("näst största tal :");    // skriv ut nästa största tal
+            input = int.Parse(Console.ReadLine());        // inmatning av inparse
+
+            
+            
+            max = input;             //   max 'r input  
+            nextmax = input;          //  next max = input
+            
+            for (int i = 0; i < 10; i++ )    //  tog for loop istället för while eftersom jag kian inte hoppa ur while loopåen
+            {
+                if( i != 0) {
+                    input = int.Parse(Console.ReadLine());    // om värden i är inte lika med 0 så ska det påbörja köra igenom loopen-
+                }
+
+                if (input > max)         // om inputen är större än max så ska max värden uträknas
+                {
+                    nextmax = max;
+                    max = input;
+                }
+
+                if ((input > nextmax && input < max) || nextmax == max)  // kganska förvirrande när input är större och när input är mindre än max
+                {                                                        // eller när next max är lika med max ska uträkning för nextma
+                    nextmax = input;
+                }
+
+                Console.WriteLine("nääst {0}   största tal {1}", nextmax, max);   //utskrivning för next max och max
+
+            }   // refrens
+
+        }
+    }
+}
