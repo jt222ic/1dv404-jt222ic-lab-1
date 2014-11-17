00307230a4cc6f8d29e4e8101801b442b8aaadf1
jantran <jt222ic@lnu.student.se>
2014-11-17 17:51:50 +0100

    git commit

---
+++ 3a/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            string length;
+            myString = Console.ReadLine();    
+            length = int.Parse(myString.Length.ToString);
+                      
+
+            for (int i = 0; i < length.Length/ 2; i++)
+            {
+                if (myString[i] == myString[i])
+                { Console.WriteLine(length); }
+                
+            }
+
+            
+            
+
+
+            
+            
+
+          
+        }
+    }
+}
