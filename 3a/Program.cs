953eb0bf9c8024b31690ff2f302b5f4f9dca58fe
jantran <jt222ic@lnu.student.se>
2014-11-20 20:38:37 +0100

    Jan tRan fixar allt!

--- 3a/Program.cs
+++ 3a/Program.cs
@@ -13,37 +13,56 @@ namespace _3a
         {
 
             int i = 0;
+            
             string str;
+            Console.Write("inmatningen sker här :");
             str = Console.ReadLine();
+            string mellan;
             
-            string presentation;
-            presentation = str;
-            str.Replace(" ","");
+            mellan = str.Replace(" ","");                   // skapar sekvenser 
             
 
-            char[] arr = str.ToCharArray();
-            Array.Reverse(arr);
+            char[] arr = mellan.ToCharArray();           // skapar char i array men räknas inte som datatypen Array
+
+          
+            
+
+           //foreach (char a in arr)              // försöker få fram character a i array 
+           //         {
+           //             Console.Write(arr[i]);           // räkna ut och läser ut för varje bokstav inom array som jag har inmatat
+                      //             ++i;                     
+                              //         }
+            bool check = false;
+           for (int p = 0; p < arr.Length; p++)
+           {    
+                                                     // går från sista framåt mot den första. första till sista
+               if (arr[p] == arr[arr.Length-1-p])        // läser sista och första bokstaven om lika räknas som panoroma
+               {
+                   Array.Reverse(arr);
+                   Console.BackgroundColor = ConsoleColor.Green;
+                   
+                  check = true;
+               }
+
+               else
+               {
+                   Console.BackgroundColor = ConsoleColor.Red;   // felmeddelandet uppkommer om det inte tillhör panorom.
+                   
+                   check = false;
+                   break;
+               }
+               
+
+              
+           }
 
 
+            if ( check == true)
+            { Console.Write("{0}-----rätt panorm", str); }
 
-
-
-            for (int p = 1; p < arr[i].length; p++)
-            {
-
-                if (arr[0] == arr[i])
-                {
-
-                    foreach (char a in arr)
-                    {
-                        Console.Write(arr[i]);
-                        ++i;
-                    }
-                }
-                
-
-
-            }
+            else
+            { Console.WriteLine("{0}------detta är Icke!  ", str); }
+            
         
     }
 
@@ -52,4 +71,4 @@ namespace _3a
 }
 
 
-//referens   //http://www.softwareandfinance.com/CSharp/Palindrome.html //
+//referens   //http://www.softwareandfinance.com/CSharp/Palindrome.html // inspiration
