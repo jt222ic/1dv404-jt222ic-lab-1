ce55d282de5304a239cbbaaae41ae67c6c882cfe
jantran <jt222ic@lnu.student.se>
2014-11-21 17:03:18 +0100

    Heejjj ska plugga hemma

---
+++ 3b/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fraction getFrac = new Fraction(1,2,3,4);
+
+
+            Console.WriteLine("En väldigt onödig funktion som skriver ut {0}", getFrac.ToString());
+
+
+        }
+    }
+
+
+    class Fraction
+    {
+        private int _Denominator;
+        private int _Numerator;
+        private int _Denominator2;
+        private int _Numerator2;
+
+
+
+        public int Denominator
+        {
+            get { return _Denominator; }
+            set
+            {
+
+                if (_Denominator == 0)
+                {
+                    throw new ArgumentException("måste ha ett tal mer än 0");
+                }
+
+                _Denominator = value;
+            }
+        }
+
+        public int Denominator2
+        {
+            get { return _Denominator2; }
+            set
+            {
+                if (_Denominator2 == 0)
+                {
+                    throw new ArgumentException("Måste ha ett tal mer än 0");
+                }
+
+                _Denominator2 = value;
+            }
+
+        }
+
+        public int Numerator2
+        {
+            get { return _Numerator2; }
+            set
+            {
+
+
+
+                _Numerator2 = value;
+            }
+
+        }
+
+        public int Numerator
+        {
+            get
+            {
+                return _Numerator;
+
+            }
+
+            set
+            {
+                _Numerator = value;
+            }
+        }
+
+        public Fraction(int denominator, int numerator, int denominator2, int numerator2)
+        {
+            Denominator = denominator;
+            Numerator = numerator;
+            Denominator2 = denominator2;
+            Numerator2 = denominator2;
+
+
+        }
+
+        public void add()
+        {
+
+            Numerator = (Denominator * Numerator2) + (Denominator2 * Numerator);
+            Denominator = Denominator * _Denominator2;
+        }
+
+
+        public void multiply()
+        {
+
+            Numerator = Numerator * Numerator2;
+            Denominator = Denominator * Denominator2;
+
+
+
+        }
+
+        public bool isEqualto()
+        {
+            int Equalo;
+            Equalo = Denominator * Numerator2;
+            Numerator = Denominator2 * Numerator;
+            if ( Equalo == Numerator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Numerator, Denominator);
+        }
+
+
+    }
+}
+
+
+
+    
+
+
+
+
+
+
---
+++ 3b/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("3b")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("3b")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14fb7a24-a2c1-4125-9747-c0838a691773")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
