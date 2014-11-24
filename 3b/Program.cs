72b4d77edd54f92b99e34bf63be557462f6f6c1d
jantran <jt222ic@lnu.student.se>
2014-11-24 19:34:24 +0100

    använde mig av lw222gz program.cs

--- 3b/Program.cs
+++ 3b/Program.cs
@@ -11,10 +11,138 @@ namespace _3b
         static void Main(string[] args)
         {
 
-            Fraction getFrac = new Fraction(1,2,3,4);
+            do
+            {
+                int a = 0;
+                int b = 0;
+                int c = 0;
+                int d = 0;
+                Fraction getfrac = null;
+
+                try
+                {
+                    Console.Clear();
+
+                    Menu();
+
+                    int choice = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (choice)
+                    {
+                        case 0:
+                            return;
+                        case 1:
+                        case 2:
+                        case 3:
+                            a = ReadFractionValue("Skriv in den första täljaren: ");
+                            b = ReadFractionValue("Skriv in den första nämnaren: ");
+                            c = ReadFractionValue("Skriv in den andra täljaren: ");
+                            d = ReadFractionValue("Skriv in den andra nämnaren: ");
+                            getfrac = new Fraction(a, b, c, d);
+                            break;
+                        case 4:
+                            a = ReadFractionValue("Skriv in täljaren: ");
+                            b = ReadFractionValue("Skriv in nämnaren: ");
+                            getfrac = new Fraction(a, b);
+                            Console.WriteLine();
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("En väldigt onödig funktion som skriver ut {0}", getfrac.ToString());
+                            Console.ResetColor();
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+
+                    }
+                    if (choice == 1)
+                    {
+                        getfrac.add();
+                        Console.WriteLine();
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("{0}/{1} + {2}/{3} = {4}", a, b, c, d, getfrac.ToString());
+                        Console.ResetColor();
+                    }
+                    else if (choice == 2)
+                    {
+                        getfrac.multiply();
+                        Console.WriteLine();
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("{0}/{1} * {2}/{3} = {4}", a, b, c, d, getfrac.ToString());
+                        Console.ResetColor();
+                    }
+                    else if (choice == 3)
+                    {
+                        if (getfrac.isEqualto() == true)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("De angivna bråken är jämna!");
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("De angivna bråken är inte jämna.");
+                            Console.ResetColor();
+                        }
+
+                    }
 
 
-            Console.WriteLine("En väldigt onödig funktion som skriver ut {0}", getFrac.ToString());
+
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("FEL! Se till så att siffran är inom intervallet och att du inte skriver in bokstäver.");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("========================================================");
+                Console.WriteLine("= Klicka valfri tangent för att göra om - ESC avslutar =");
+                Console.WriteLine("========================================================");
+                Console.ResetColor();
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+        }
+
+
+        static void Menu()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("===================================================");
+            Console.WriteLine("=                                                 =");
+            Console.WriteLine("=                    Bråk tal                     =");
+            Console.WriteLine("=                                                 =");
+            Console.WriteLine("===================================================");
+            Console.ResetColor();
+            Console.WriteLine("\n 0.Avsluta.\n\n 1. Addera 2 bråktal\n\n 2. Multiplicera 2 bråktal\n\n 3. Jämnaföra 2 bråktal\n\n 4. Bara skriva in ett bråktal och få det utskrivet");
+            Console.WriteLine("===================================================");
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+
+        static int ReadFractionValue(string promt)
+        {
+            int value;
+
+            while (true)
+            {
+                Console.Write(promt);
+                string input = Console.ReadLine();
+                try
+                {
+                    value = int.Parse(input);
+                    return value;
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine();
+                    Console.WriteLine("FEL! {0} är inte i önskat format, var vänlig och ange ett tal.", input);
+                    Console.ResetColor();
+                }
+            }
 
 
         }
@@ -85,6 +213,12 @@ namespace _3b
             {
                 _Numerator = value;
             }
+        }
+
+        public Fraction(int numerator, int denominator)
+            : this(numerator, denominator, 1, 1)
+        {
+
         }
 
         public Fraction(int denominator, int numerator, int denominator2, int numerator2)
